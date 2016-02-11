b70b5897e1aebb9ed4a0a0d7d37644ee14ae9410
Adam James-Liebau Johnson <riizade@gmail.com>
2016-02-10 19:16:40 -0500

    portals now indicate when an enemy is spawning

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class EnemySpawner : MonoBehaviour {
 
     public float spawnInterval = 3f; // how often (in seconds) the spawner can spawn an enemy
+    public float spawnDelay = 1f; // how long it takes to spawn an enemy
 
     private Queue<GameObject> spawnQueue = new Queue<GameObject>(); // the enemies that are queued to spawn
 
@@ -18,13 +19,33 @@ public class EnemySpawner : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         // spawns an enemy at the spawn interval
-        InvokeRepeating("SpawnEnemy", 0, spawnInterval);
+        InvokeRepeating("ManageSpawn", 0, spawnInterval);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void ManageSpawn() {
+        // if the queue is empty, do nothing
+        if (spawnQueue.Count == 0) {
+            return;
+        }
+        SpawnEffectOn();
+        Invoke("SpawnEffectOff", this.spawnDelay);
+        Invoke("SpawnEnemy", this.spawnDelay);
+    }
+
+    // turns on the spawn effect
+    void SpawnEffectOn() {
+        this.transform.GetChild(1).gameObject.GetComponent<ParticleSystem>().Play();
+    }
+
+    // turns off the spawn effect
+    void SpawnEffectOff() {
+        this.transform.GetChild(1).gameObject.GetComponent<ParticleSystem>().Stop();
+    }
 
     // add an enemy to the spawn queue
     public void QueueEnemy(GameObject enemy) {
@@ -34,9 +55,9 @@ public class EnemySpawner : MonoBehaviour {
     public void SpawnEnemy() {
         // if the queue is empty, do nothing
         if (spawnQueue.Count == 0) {
+            print("Tried to spawn an enemy with nothing in the spawn queue!");
             return;
         }
-
         // spawn an enemy
         GameObject enemy = spawnQueue.Dequeue();
         GameObject enemyGO = Instantiate(enemy);
