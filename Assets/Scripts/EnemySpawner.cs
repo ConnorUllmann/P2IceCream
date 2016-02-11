ac033210d1a0006a64c211dbe783a7c18743e270
ConnorUllmann <c.ullmann@hotmail.com>
2016-02-11 14:58:17 -0500

    Modified spawner

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class EnemySpawner : MonoBehaviour {
 
     public float spawnInterval = 3f; // how often (in seconds) the spawner can spawn an enemy
+    //public float spawnDelay = 1f; // how long it takes to spawn an enemy
 
     private Queue<GameObject> spawnQueue = new Queue<GameObject>(); // the enemies that are queued to spawn
 
@@ -25,7 +26,11 @@ public class EnemySpawner : MonoBehaviour {
         stretch = stretchMin;
         UpdatePortalSize();
         // spawns an enemy at the spawn interval
+/*<<<<<<< HEAD
         //InvokeRepeating("SpawnEnemy", 0, spawnInterval);
+=======
+        InvokeRepeating("ManageSpawn", 0, spawnInterval);
+>>>>>>> origin/master*/
 	}
 
     public GameObject portalEffect;
@@ -84,6 +89,26 @@ public class EnemySpawner : MonoBehaviour {
         fireBackEffect.transform.localScale = new Vector3(fireBackStartScale.x * n, fireBackStartScale.y, fireBackStartScale.z);
     }
 
+    /*void ManageSpawn() {
+        // if the queue is empty, do nothing
+        if (spawnQueue.Count == 0) {
+            return;
+        }
+        SpawnEffectOn();
+        Invoke("SpawnEffectOff", this.spawnDelay);
+        Invoke("SpawnEnemy", this.spawnDelay);
+    }
+
+    // turns on the spawn effect
+    void SpawnEffectOn() {
+        this.transform.GetChild(1).gameObject.GetComponent<ParticleSystem>().Play();
+    }
+
+    // turns off the spawn effect
+    void SpawnEffectOff() {
+        this.transform.GetChild(1).gameObject.GetComponent<ParticleSystem>().Stop();
+    }*/
+
     // add an enemy to the spawn queue
     public void QueueEnemy(GameObject enemy) {
         spawnQueue.Enqueue(enemy);
@@ -92,9 +117,9 @@ public class EnemySpawner : MonoBehaviour {
     public void SpawnEnemy() {
         // if the queue is empty, do nothing
         if (spawnQueue.Count == 0) {
+            print("Tried to spawn an enemy with nothing in the spawn queue!");
             return;
         }
-
         // spawn an enemy
         GameObject enemy = spawnQueue.Dequeue();
         GameObject enemyGO = Instantiate(enemy);
